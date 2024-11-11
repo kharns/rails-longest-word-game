650c9a8d5f96878ecc53a84793b30adeaa1dc4ab
kristen harnois <kristen.harnois@gmail.com>
2024-11-11 13:02:57 +0100

    page new ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+
+  def new
+    @letters = []
+    10.times do
+      @letters << [*('a'..'z')].sample.upcase
+    end
+    return @letters
+  end
+
+  def score
+    @word = params[:word]
+    # Implémenter la logique du jeu
+    # Le mot ne peut pas être créé à partir de la grille d’origine.
+    # Le mot est valide d’après la grille, mais ce n’est pas un mot anglais valide.
+    # Le mot est valide d’après la grille et est un mot anglais valide.
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "new", to: "games#new"
-  get "score", to:"games#score"
+  post "score", to:"games#score"
 
 end
