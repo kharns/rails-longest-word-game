69a57cafcb32eba58d9c7fe1afc5e358ea3e28e1
kristen harnois <kristen.harnois@gmail.com>
2024-11-11 15:50:28 +0100

    exercice ok avec score total

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class GamesController < ApplicationController
 
   def new
@@ -9,11 +12,37 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word = params[:word]
-    # Implémenter la logique du jeu
-    # Le mot ne peut pas être créé à partir de la grille d’origine.
-    # Le mot est valide d’après la grille, mais ce n’est pas un mot anglais valide.
-    # Le mot est valide d’après la grille et est un mot anglais valide.
-  end
+    # on définie ce dont on a besoin
+    @word = params[:word].upcase.strip
+    @letters = params[:letters]
+    @verif = []
 
+    # parsing de l'API en json
+    url = "https://dictionary.lewagon.com/#{@word}"
+    url_serialized = URI.parse(url).read
+    dictionnary = JSON.parse(url_serialized)
+
+    # Implémenter la logique du jeu
+    # @word.split => pour chaque lettre on vérifie le nombre d'occurences dans @word <= au nombre dans @letters
+    @word.split('').each do |letter|
+      @verif << (@letters.include?(letter) && @word.count(letter) <= @letters.count(letter) ? "true" : "false")
+    end
+
+    # Le mot ne peut pas être créé à partir de la grille d’origine.
+    if @verif.include?("false")
+      @message_to_user = "Sorry but #{@word.upcase} can't be build out of #{@letters}"
+      @score = 0
+    # Le mot est valide d’après la grille, mais ce n’est pas un mot anglais valide.
+    elsif dictionnary["found"] == false
+      @message_to_user = "Sorry but #{@word.upcase} does not seemto be an English word!"
+      @score = 0
+    # Le mot est valide d’après la grille et est un mot anglais valide.
+    else
+      @message_to_user = "Congratulations! #{@word.upcase} is a valid English word!"
+      @score = @word.length ** 2
+    end
+    
+  # calcul du total de points de l'utilisateur
+  session[:total_score] == nil ? session[:total_score] = @score : session[:total_score] += @score
+  end
 end
